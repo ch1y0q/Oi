d17fa317733fd0f93cef56369acb706fe85475fa
Bill Li <sa1284792303@outlook.com>
2017-01-20 22:35:11 +0800

    master

---
+++ other/BaHuangHou.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+bool a[8][8];
+bool hang(int x;int y);
+bool lie(int x,int y);
+bool xie(int x,int y);
+
+int main()
+{
+
+}
+
+bool hang(int x;int y)
+{
+    
+}
+bool lie(int x,int y)
+{
+
+}
+bool xie(int x,int y)
+{
+
+}
