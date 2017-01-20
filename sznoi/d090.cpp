41291ccfc1a5a70bc93e758bb3958cdc92cc04e8
Bill Li <sa1284792303@outlook.com>
2017-01-20 15:26:01 +0800

    master

---
+++ sznoi/d090.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int l1,l2;
+    string s1,s2;
+    cin>>s1>>s2;
+    l1=s1.length();
+    l2=s2.length();
+    int k[l1];
+    int j[l2];
+    for(int i=0;i<l1;i++)
+        k[i]=s1[i]-48;
+    for(int i=0;i<l2;i++)
+        j[i]=s2[i]-48;
+    
+    for()
+
+
+    return 0;
+}
