03f1321b0aff2e342e970227b6ae9d4d4e4719fa
Bill Li <sa1284792303@outlook.com>
2017-01-21 14:26:38 +0800

    master

---
+++ Other/TiKuPiPei.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+using namespace std;
+
+FILE *in,*out;
+
+void do_it();
+void find_title();
+void find_problem();
+
+int main()
+{
+    out=fopen("problem.html","a");
+    do_it();
+    return 0;
+}
+
+void do_it()
+{
+    in=fopen("showproblem-problem_id=1600.html","r"); //仅测试，改为多个文件批量读取
+    void find_title();
+}
+
+void find_title()
+{
+    char title_begian[]="<title>";
+    char title_end[]="</title>";
+    
+}
