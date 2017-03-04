3ad16933a19c4803aa9f489c116a4eff1aaac0ec
sa1284792303 <sa1284792303@outlook.com>
2017-03-05 02:20:41 +0800

    master

---
+++ Other/bfs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#define Maxn 6
+
+using namespace std;
+
+void bfsint(int cur);
+
+int bmp[Maxn][Maxn];
+
+int main()
+{
+    freopen("../data.in","r",stdin);
+    for(int i=0;i<Maxn;)
+}
+
+void bfs(int cur)
+{
+
+}
