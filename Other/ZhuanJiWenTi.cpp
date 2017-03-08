c86aa03fa08369a381f365ef2631a2e087b787e1
sa1284792303 <sa1284792303@outlook.com>
2017-03-08 16:47:55 +0800

    master

--- Other/ZhuanJiWenTi.cpp
+++ Other/ZhuanJiWenTi.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
+bool cmp(hx a,hx b);
+
+struct hx{
+    int start,end;
+};
+
 int main()
 {
-    int flag;
+    freopen("data.in","r",stdin);
+    freopen("data.out","w",stdout);
+    int flag=0,a,b,c,d,cur;
+    cin>>a>>b>>c>>d;
+    hx[b];
+    for(int i=0;i<b;i++)
+    cin>>hx[i];
+    sort(hx,hx+b,cmp);
+    for(int i=0;i<b;i++)
+    {
+        if(hx[i].start==c)
+        cur=hx[i].end;
+        flag++;
+    }
+
+}
+
+bool cmp(hx a,hx b)
+{
+    if a.start==b.start
+        return a.end<b.end;
+    else
+        return a.start<b.start;
 }
