d1324c3a42f8c94dcc345bf9659fe704649e183f
sa1284792303 <sa1284792303@outlook.com>
2017-03-08 11:42:37 +0800

    master

--- USACO/USACO1.2 Palindromic Squares.cpp
+++ USACO/USACO1.2 Palindromic Squares.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main()
 {
+    freopen("data.in","r",stdin);
+    freopen("data.out","w",stdout);
+    //freopen("palsquare.in","r",stdin);
+    //freopen("palsquare.out","w",stdout);
     int n;
     cin>>n;
+    cout<<n;
 }
