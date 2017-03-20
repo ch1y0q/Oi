81b6fa4fd2e82f3ac97f69d2e1fdcec44377b53b
sa1284792303 <sa1284792303@outlook.com>
2017-03-20 12:47:19 +0800

    master

--- CodeVS/p2806.cpp
+++ CodeVS/p2806.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
 #define Maxn 21
 
 using namespace std;
@@ -6,11 +8,12 @@ using namespace std;
 char pic[Maxn][Maxn];
 int m,n,idx[Maxn][Maxn],cnt;
 
-void dfs(int i,j);
+void dfs(int i,int j);
 
 int main()
 {
-    while(scanf("%d%d",m,n)==2 && m!=0 && n!=0 )
+    freopen("data.in","r",stdin);
+    while(scanf("%d%d",&n,&m)==2 && m!=0 && n!=0 )
     {
         for(int i=0;i<m;i++)
             scanf("%s",pic[i]);
@@ -19,7 +22,21 @@ int main()
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
                 if(pic[i][j]=='@')
+                {
+                    cout<<"老子DFS了"<<endl;
                     dfs(i,j);
+                }
+        cout<<cnt<<endl;
     }
     return 0;
+}
+
+void dfs(int i,int j)
+{
+    if(i<0 || j<0 || i>=n || j>=m || idx[i][j] || pic[i][j]=='#')
+        return;
+    dfs(i-1,j);
+    dfs(i+1,j);
+    dfs(i,j-1);
+    dfs(i,j+1);
 }
