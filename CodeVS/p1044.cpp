64c8da20af4ffcc40952dd2f18f289cdc952bfd5
sa1284792303 <sa1284792303@outlook.com>
2017-03-26 17:00:59 +0800

    master

--- CodeVS/p1044.cpp
+++ CodeVS/p1044.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
-int dd[25];
+int dd[25],nmax,flag1=1,flag2=1;
 
 int main()
 {
-
+    freopen("data.in","r",stdin);
+    int n=-1;
+    while(scanf("%d",&dd[++n])==1){}
+    
+    cout<<flag1<<" "<<flag2<<endl;
     return 0;
 }
