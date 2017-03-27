f867b379e95ae04b4fd6a93b7673d329d0d6bcd1
sa1284792303 <sa1284792303@outlook.com>
2017-03-27 13:01:42 +0800

    master

--- CodeVS/p1044.cpp
+++ CodeVS/p1044.cpp
@@ -1,38 +1,28 @@
 #include <cstdio>
 #include <iostream>
+#include <cstring>
 
 #define INF 2147483647 //最大int
 
 using namespace std;
 
-int dd[25],nmax,nmin,flag1=1,flag2=1,height[30001],dp[30],n=-1;
-
-int lj(int n);
-int xt(int n);
+int dd[25],nmax,flag,dp[30],n=-1;
 
 int main()
 {
     freopen("data.in","r",stdin);
     while(scanf("%d",&dd[++n])==1){}
     
-    printf("%d %d",lj(n),xt(n));
-    return 0;
-}
-
-int lj(int n)
-{
-    for (int i = 0; i < n; i++) 
+    for(int i=0;i<n;i++)
     {
-		int nmax;
-        dp[i] = 1;
+        dp[i]=1;
         for(int j=0;j<i;j++)
-            if(dd[j]>dd[i])
+            if(dd[j]>=dd[i])
                 dp[i]=max(dp[i],dp[j]+1);
-	}
-    return nmax;
-}
-
-int xt(int n)
-{
-    cout<<"cat";
+        nmax=max(nmax,dp[i]);
+    }
+    cout<<nmax<<" ";
+    memset(dp,0,dp+30);
+    for(int i=1;i<n;i++)
+    return 0;
 }
